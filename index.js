50be7f7e6d0963ac2c873967a0288455bd63ff46
sanfarfreezeco <farizcocoa@live.com>
2023-03-22 21:22:49 +0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const qrcode = require('qrcode-terminal');
+const { Client } = require('whatsapp-web.js');
+
+const client = new Client();
+client.initialize();
+
+client.on('qr', (qr) => {
+    qrcode.generate(qr, { small: true });
+});
+
+client.on('ready', () => {
+    console.log('ready!');
+});
