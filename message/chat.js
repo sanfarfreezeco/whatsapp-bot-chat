d617fccd783abb74c89faf9d4bed2d4358070940
sanfarfreezeco <farizcocoa@live.com>
2023-03-31 13:44:48 +0700

    Update: Fix read JSON >15 and add date log on JSON

--- message/chat.js
+++ message/chat.js
@@ -5,6 +5,7 @@ const OPENAI_API = process.env.OPENAI_API;
 const configuration = new Configuration({ apiKey: OPENAI_API });
 const openai = new OpenAIApi(configuration);
 const fs = require('fs');
+const date = require('date-and-time');
 
 function chat() {
     client.on('message', async (message) => {
@@ -14,18 +15,23 @@ function chat() {
         if (message.body !== '/start' &&
             message.body !== '/stop' &&
             message.body !== '/test' &&
-            message.body !== '/test2'
-        ) {
+            message.body !== '/test2' &&
+            message.body !== '/test3') {
             if (fs.existsSync(file)) {
                 let messages;
                 let messagesOri = [];
+                const now = new Date();
+                const calendar = date.format(now, 'dddd, MMMM DD YYYY HH:mm:ss.SSS [UTC]ZZ');
                 const prevFile = fs.readFileSync(file);
                 const parseFile = JSON.parse(prevFile);
-                if (parseFile <= !15) {
-                    messagesOri = parseFile.slice(0, -15);
-                    messages = parseFile.slice(-15);
+                let chatDate = parseFile.slice(0, 1);
+                chatDate[0].last_chatting = calendar;
+                const chat = parseFile.slice(1, parseFile.length);
+                if (chat.length > 15) {
+                    messagesOri = chat.slice(0, -15);
+                    messages = chat.slice(-15);
                 } else {
-                    messages = parseFile;
+                    messages = chat;
                 }
                 try {
                     const input = message.body;
@@ -39,12 +45,16 @@ function chat() {
                     });
                     const reply = completion.data.choices[0].message;
                     messages.push(reply);
-                    if (messagesOri <= !15) {
+                    if (chat.length > 15) {
+                        chatDate = chatDate.concat(messagesOri);
+                        messagesOri = chatDate;
                         messagesOri = messagesOri.concat(messages);
                         const json = JSON.stringify(messagesOri, null, 4);
                         fs.writeFileSync(file, json);
                     } else {
-                        const json = JSON.stringify(message, null, 4);
+                        chatDate = chatDate.concat(messages);
+                        messages = chatDate;
+                        const json = JSON.stringify(messages, null, 4);
                         fs.writeFileSync(file, json);
                     }
                     client.sendMessage(message.from, reply.content);
--- message/commands.js
+++ message/commands.js
@@ -1,16 +1,22 @@
 const { client } = require('../client');
 const fs = require('fs');
+const date = require('date-and-time');
 
 function commands() {
     client.on('message', async (message) => {
         const contact = await message.getContact();
         const file = 'message/messageLogs/' + contact.id.user;
+        const now = new Date();
+        const calendar = date.format(now, 'dddd, MMMM DD YYYY HH:mm:ss.SSS [UTC]ZZ');
         if (message.body === '/start') {
             if (fs.existsSync(file)) {
                 client.sendMessage(message.from, 'You are already started a chat');
             } else {
-                const newMessages = fs.readFileSync('message/messageLogs/template');
-                fs.writeFileSync(file, newMessages);
+                const newMessages = JSON.parse(fs.readFileSync('message/messageLogs/template'));
+                const createDate = {"create_at": calendar, "last_chatting": calendar, "stopped_at": "null"};
+                newMessages.splice(0, 0, createDate);
+                const json = JSON.stringify(newMessages, null, 4);
+                fs.writeFileSync(file, json);
                 client.sendMessage(message.from, 'Ready!\nUse /stop to stop chat');
             }
         }
@@ -25,8 +31,10 @@ function commands() {
                 }
                 i++;
             }
-            const oldMessages = fs.readFileSync(file);
-            fs.writeFileSync(fileOld, oldMessages);
+            const oldMessages = JSON.parse(fs.readFileSync(file));
+            oldMessages[0].stopped_at = calendar;
+            const json = JSON.stringify(oldMessages, null, 4);
+            fs.writeFileSync(fileOld, json);
             fs.rmSync(file);
             client.sendMessage(message.from, 'Chat was stopped');
             client.sendMessage(message.from, 'To start chat again please use /start again');
