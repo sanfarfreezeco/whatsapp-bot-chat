7383475873d2d3b2f27a0b10287570580d71e367
sanfarfreezeco <farizcocoa@live.com>
2023-04-01 23:35:21 +0700

    Initial Commit

--- message/test.js
+++ message/test.js
@@ -5,9 +5,18 @@ const OPENAI_API = process.env.OPENAI_API;
 const configuration = new Configuration({ apiKey: OPENAI_API });
 const openai = new OpenAIApi(configuration);
 const fs = require('fs');
+const date = require('date-and-time');
 
 function testMsg() {
     client.on('message', async (message) => {
+        if (message.body === '/test') {
+        }
+
+        if (message.body === '/test2') {
+        }
+
+        if (message.body === '/test3') {
+        }
     });
 }
 
