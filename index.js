197c02dd63185ddd3278e8d26de7abb349decd4d
sanfarfreezeco <farizcocoa@live.com>
2023-04-02 19:07:53 +0700

    Update: Add greeting message while joining group chat

--- index.js
+++ index.js
@@ -14,5 +14,11 @@ client.on('ready', () => {
     console.log('ready!');
 });
 
+client.on('group_join', async (group) => {
+    await client.sendMessage(group.chatId, 'Hello i\'m ' + client.info.pushname);
+    await client.sendMessage(group.chatId, 'I\'m a bot\nSo you can chat with me in this group with /start command\nYour chat will not interrupted by me until you start the chat using /start command');
+    await client.sendMessage(group.chatId, 'You can stop chat with me in this group with /stop command\nSo you can talk each other without interrupted by me');
+});
+
 const { loader } = require('./msgLoader');
 loader();
