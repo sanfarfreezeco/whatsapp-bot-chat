404afdb53d7e28e4fe890615556d96d9c2bc0f6d
sanfarfreezeco <farizcocoa@live.com>
2023-04-02 20:04:13 +0700

    Update: Add DM message to who removed bot to group chat

--- index.js
+++ index.js
@@ -20,5 +20,10 @@ client.on('group_join', async (group) => {
     await client.sendMessage(group.chatId, 'You can stop chat with me in this group with /stop command\nSo you can talk each other without interrupted by me');
 });
 
+client.on('group_leave', async (group) => {
+    const groupChat = (await group.getChat()).name;
+    await client.sendMessage(group.author, 'You just removed me from ' + groupChat + '\nIf you want to invite me back to your group. I\'ll feel free to accept it');
+});
+
 const { loader } = require('./msgLoader');
 loader();
